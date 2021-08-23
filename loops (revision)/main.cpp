4170e62df54fb1b003d79f695a81deda65190a2f
Martin Ahoto <martinahoto4@gmail.com>
2021-08-23 18:59:44 +0300

    basic revised source codes on loops

--- loops (revision)/main.cpp
+++ loops (revision)/main.cpp
@@ -1,14 +1,28 @@
 //
 //  main.cpp
 //  loops (revision)
+// Question(output the multiplication table for a user defined number)
 //
 //  Created by MARTIN on 23/08/2021.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int limit=10;
+    int count=0;
+    int num;
+    
+    cout<<"enter the number here"<<endl;
+    cin>>num;
+    
+    while(count<=limit)
+    {
+        cout<<count<<"*"<<num<<"="<<count*num<<endl;
+        count++;
+    }
+    
     return 0;
 }
